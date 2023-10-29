c38b2b9b587d28d25d30759a461fe0f4d32fdbf8
원치현 <58906858+chihyunwon@users.noreply.github.com>
2023-10-29 20:33:42 +0900

    Add files via upload

--- area.c
+++ area.c
@@ -1,75 +1,102 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
-#include <sys/time.h> // gettimeofday 함수를 사용하기 위함
+#include <mpi.h>
+#include <sys/time.h>
+
+long millisecond()
+{
+   struct timeval tv;
+   gettimeofday(&tv, NULL);
+   return(1000*tv.tv_sec + tv.tv_usec/1000) ;
+}
 
 double f(double x)
 {
    double temp;
-   temp = x*x+2; // 1곱하기 1덧셈 -> 2
-   return x/(temp*temp*temp); // 2곱하기 1나눗셈->3 함수 호출 때마다 총 개
-}
-
-long milliseconds()
-{
-  struct timeval tv;
-  gettimeofday(&tv, NULL);
-  return (long)(tv.tv_sec) * 1000 + (long)(tv.tv_usec) / 1000;
+   temp = x*x+2;
+   return x/(temp*temp*temp);
 }
 
 int main(int argc, char *argv[])
-{ 
-   int i;
-   long N; // loops 수
-   long I;
-   int flops; // flops 수
-   double a, b, area, dx, x;
-
+{
+   long N, i;
+   double a, b, area, local_area, dx, x;
    long start, elapsed;
-  
+   int nt, tid;
 
-   N = atol(argv[1]); // area 뒤에오는 수를 loops 수로 저장
+   int pid, size;
 
-
-
-   if(argc != 2)	{
-      printf("argument error\n");
-      return 1;
-   }
-
-   // 시작 시간 기록
-   start = milliseconds();   
+   MPI_Init(&argc,&argv);
+   MPI_Comm_rank(MPI_COMM_WORLD,&pid);
+   MPI_Comm_size(MPI_COMM_WORLD,&size);
    
-   a = 0.0; // 구간 시작
-   b = 2.0; // 구간 끝
-   dx = (b-a)/N;
+   if (argc != 2) {
+      printf("usage:%s N\n", argv[0]);
+      exit(1);
+   }            
+
+   N = atol(argv[1]);
+
+   // sequential calculation of area
+   start = millisecond();
+   a = 0.0;
+   b = 2.0;
+   dx = (b-a)/(double)N;
 
    area = 0.0;
 
    x = a;
-   
-   flops = 15;
-   
-    for(i=0; i<N; i++) {
-      area += 0.5*(f(x)+f(x+dx))*dx; // f 호출->5 * 2= 12, 덧셈 2 곱셈 2 = 14  
-      x += dx; // 덧셈 1 -> 14 + 1 = 15 
+   for (i=0; i<N; i++) {
+      area += 0.5*(f(x)+f(x+dx))*dx;
+      x += dx;
    }
-   // 종료 시간 기록
-   elapsed = milliseconds() - start;
-   
-   // the calculated area 값 계산
-   printf("the calculated area is %5.10lf\n", area);
 
-   // 계산한 elapsed time 결과 출력
-   printf("Elapsed Time is %lf ms.\n", elapsed);
-   
-   // GFLOPS 계산
-   long total_operations = N * flops; // 총 연산 수 계산
-   long gflops = (total_operations / (elapsed / 1000.0)) * 1e-9; // GFLOPS 계산
+   elapsed = millisecond() - start;
 
-   // GFLOPS 결과 출력
-   printf("GFLOPS: %lf\n", gflops);
+   printf("%5.10lf\n", area);
+   printf("elapsed time: %ld milliseconds\n", elapsed);
+   printf("GFLOPS: %10.2f\n", (N*16.0/(elapsed/1000.0))/1000000000.0);
+
+
+   // calculates local area
+   MPI_Barrier(MPI_COMM_WORLD);
+   start = millisecond();
+
+   double local_a = ((b-a)/size)*pid;
+   double local_b = ((b-a)/size)*(pid+1);
+
+   local_area = 0.0;
    
-   return 0;
-}    
+    
+   x = local_a;
+   for(i=0; i<N; i++) {
+      local_area += 0.5*(f(x)+f(x+dx))*dx;
+      x += dx;
+   }
+
+   // add local_areas onto area on p0 (pid==0. tag = 17)
+   if(pid == 0) {
+      for(i=1; i<size; i++) {
+         MPI_Recv(&local_area, 1, MPI_DOUBLE, i, 17, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+         area += local_area;
+      }
+      
+      area += local_area;
+      
+   } else {
+      MPI_Send(&local_area, 1, MPI_DOUBLE, 0, 17, MPI_COMM_WORLD);
+   }
+
+   elapsed = millisecond() - start;
+
+   if(pid == 0) {
+      // print elapsed time and GFLOPS
+      printf("elapsed time(MPI): %ld milliseconds\n", elapsed);
+      printf("GFLOPS(MPI): %5.2f\n", (N*12.0/(elapsed/1000.0))/1000000000.0); 
+   }
+
+   MPI_Finalize();
+
+   exit(0);
+}
